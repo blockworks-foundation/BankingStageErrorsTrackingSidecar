63edab3060d2e89358349315492b0492743f6f87
galactus <96341601+godmodegalactus@users.noreply.github.com>
2023-12-05 19:11:18 +0100

    chaning primary key on transaction infos table (#11)

--- migration.sql
+++ migration.sql
@@ -2,16 +2,17 @@ CREATE SCHEMA banking_stage_results;
 
 CREATE TABLE banking_stage_results.transaction_infos (
   signature CHAR(88) PRIMARY KEY,
+  first_notification_slot BIGINT NOT NULL,
   errors text,
   is_executed BOOL,
   is_confirmed BOOL,
-  first_notification_slot BIGINT NOT NULL,
   cu_requested BIGINT,
   prioritization_fees BIGINT,
   utc_timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
   accounts_used text,
   processed_slot BIGINT,
-  supp_infos text
+  supp_infos text,
+  Primary key (signature, first_notification_slot)
 );
 
 CREATE TABLE banking_stage_results.blocks (
